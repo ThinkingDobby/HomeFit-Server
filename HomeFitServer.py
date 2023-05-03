654550c6c38c18b293f63f2c64b03203c53a0b02
ThinkingDobby <chiho1221@naver.com>
2023-05-03 21:59:48 +0900

    JSON파일 전송 구현

--- HomeFitServer.py
+++ HomeFitServer.py
@@ -103,17 +103,12 @@ class HomeFitServer:
                 resultMessage.setValue(fileSize)
                 clientSocket.sendall(resultMessage.getResultMessage(33))
 
-                # 아래 코드 참고해 파일 전송 구현
-                # print("파일 %s 전송 시작" %filename)
-                # with open(filename, 'rb') as f:
-                #     try:
-                #         data = f.read(1024) #1024바이트 읽는다
-                #         while data: #데이터가 없을 때까지
-                #             data_transferred += connectionSock.send(data) #1024바이트 보내고 크기 저장
-                #             data = f.read(1024) #1024바이트 읽음
-                #     except Exception as ex:
-                #         print(ex)
-                # print("전송완료 %s, 전송량 %d" %(filename, data_transferred))
+                dataTransferred = 0
+                with open(filePath, 'rb') as f:
+                    data = f.read(1024)
+                    while data:
+                        dataTransferred += clientSocket.send(data)
+                        data = f.read(1024)
 
                 print("transmission started")
 
